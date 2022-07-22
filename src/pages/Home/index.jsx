d19a3330ca5da04c8ae840b0f8a6ade30d176958
Gabriel da Silva Brito <gabrieldsb95@gmail.com>
2022-07-22 18:13:15 -0300

    first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import './style.css';
+
+function Home() {
+
+  return (
+    <div className='container'>
+      <h1>Lista de Presença</h1>
+      <input type="text" placeholder="Digite um nome"></input>
+      <button type="button">Adicionar</button>
+    </div>
+  )
+}
+
+export default Home
