15e66fe21de0791c264e4bf0559164793f31b492
Gabriel <gabrieldsb95@gmail.com>
2022-07-23 10:23:45 -0300

    add cards

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import './style.css'
+
+export function Card(){
+    return (
+        <div className='card'>
+            <strong>A train</strong>
+            <small>10:00:01</small>
+        </div>
+    )
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,17 @@
 import './style.css';
+import {Card} from '../../components/Card'
 
-function Home() {
+export function Home() {
 
   return (
     <div className='container'>
       <h1>Lista de Presença</h1>
       <input type="text" placeholder="Digite um nome"></input>
       <button type="button">Adicionar</button>
+
+      <Card />
+      <Card />
+      <Card />
     </div>
   )
 }
-
-export default Home
